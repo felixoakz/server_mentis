2ca0d08efc41f0b6e5396d3bc243c4c4410dc82b
felix <felix@oakz.org>
2025-01-05 17:08:25 -0300

    added user model and migration with sequelize

---
+++ src/models/user.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const { Model } = require('sequelize');
+
+
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true,
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [8, 16],
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: 'User',
+    }
+  );
+
+  return User;
+};
