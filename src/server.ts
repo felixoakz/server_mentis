c52ab6917235e65bb88138a72abacde6371b3c97
felix <felix@oakz.org>
2025-01-06 20:04:48 -0300

    added cors

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,18 @@ import express from 'express';
 import { PORT } from './config/dotenv';
 import authRoutes from './routes/auth';
 import authMiddleware from './middleware/auth';
+import cors from 'cors'
 
 const app = express();
 
 app.use(express.json()); // Parse incoming JSON
 
+app.use(
+  cors({
+    origin: 'http://localhost:3001', // Replace with your React app URL
+    credentials: true, // Allow cookies if required
+  })
+);
 
 // Start the server
 app.listen(PORT, () => {
