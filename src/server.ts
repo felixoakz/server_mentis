2bc69eceef33564251eb867a9de182acbf157c71
felix <felix@oakz.org>
2025-02-17 20:44:30 -0300

    tidy up on server file

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import Fastify, { FastifyInstance } from "fastify";
 import fastifyCookie from "@fastify/cookie";
 import fastifyJwt from "@fastify/jwt";
-import rateLimit from "@fastify/rate-limit";
+import fastifyRateLimit from "@fastify/rate-limit";
 import chalk from "chalk";
 
 import { loggingMiddleware } from "./middlewares/loggingMiddleware";
@@ -10,38 +10,34 @@ import accountRoute from "handlers/account/accountRoutes";
 import authRoute from "./handlers/auth/authRoutes";
 import transactionRoute from "handlers/transaction/transactionRoutes";
 
+const { cookieSecret, jwtSecret, rateLimit, port } = constants;
 
 const fastify: FastifyInstance = Fastify();
 
 // Register plugins
-fastify.register(fastifyCookie, { secret: constants.cookieSecret });
-
-fastify.register(fastifyJwt, {
-  secret: constants.jwtSecret,
-  cookie: { cookieName: "token", signed: true, },
-});
-
-fastify.register(rateLimit, {
-  max: constants.rateLimit.max,
-  timeWindow: constants.rateLimit.timeWindow,
-});
+fastify.register(fastifyCookie, { secret: cookieSecret });
+fastify.register(fastifyJwt, { secret: jwtSecret, cookie: { cookieName: "token", signed: true } });
+fastify.register(fastifyRateLimit, { max: rateLimit.max, timeWindow: rateLimit.timeWindow });
 
 // Middleware
 fastify.addHook("onRequest", loggingMiddleware);
 
 // Routes
-fastify.register(authRoute, { prefix: "/auth" });
-fastify.register(accountRoute, { prefix: "/api" });
-fastify.register(transactionRoute, {prefix: "/api"});
+const routes = [
+  { route: authRoute, prefix: "/auth" },
+  { route: accountRoute, prefix: "/api" },
+  { route: transactionRoute, prefix: "/api" },
+];
+
+routes.forEach(({ route, prefix }) => fastify.register(route, { prefix }));
 
 // Start server
-const start = async () => {
+const start = async (): Promise<void> => {
   try {
-    await fastify.listen({ port: constants.port });
-    console.log(chalk.bgGray(`\n===> Server is running on http://localhost:${constants.port}`));
-
+    await fastify.listen({ port });
+    console.log(chalk.bgGray(`\n===> Server is running on http://localhost:${port}`));
   } catch (err) {
-    console.log(chalk.bgRed(err))
+    console.error(chalk.bgRed(err));
     fastify.log.error(err);
     process.exit(1);
   }
